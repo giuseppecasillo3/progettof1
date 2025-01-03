1ac3a75da5f93399b3a4e67bdde24e6f47db4f53
Giuseppe Casillo <giuseppe.casillo3@gmail.com>
2025-01-03 15:45:59 +0100

    menu aggiornato

--- menu.py
+++ menu.py
@@ -1,25 +1,110 @@
-import funzioni
+from funzioni import *
+from progetto import*
+
+def menu_scuderia():
+    while True:
+        print("Menu:")
+        print("1. Piloti che hanno corso nella scuderia")
+        print("2. Andamento punti scuderia su un circuito specifico")
+        print("3. Andamento punti scuderia nelle stagioni")
+        print("4. Torna indietro")
+        print("5. Esci")
+        scelta = int(input("Inserisci il numero corrispondente all'operazione desiderata: "))
+        if scelta == 1:
+                nome_scuderia = input("inserisci il nome della scuderia: ").strip()
+                Piloti_per_scuderia(nome_scuderia)
+        if scelta == 2:
+            nome_scuderia = input("Inserisci il nome della scuderia: ").strip()
+            nome_pista = input("Inserisci il nome del circuito: ").strip()
+            circuit_id = Cerca_ID_circuito(nome_pista)
+            constructor_id = Cerca_ID_scuderia(nome_scuderia)
+            if constructor_id is not None and circuit_id is not None:
+                grafico_punti_scuderia(constructor_id, circuit_id)
+            else:
+                print("Scuderia o circuito non trovato.")
+        elif scelta == 3:
+            nome_scuderia = input("Inserisci il nome della scuderia: ").strip()
+            constructor_id = Cerca_ID_scuderia(nome_scuderia)
+            if constructor_id is not None:
+                grafico_punti_totali_scuderia(constructor_id)
+            else:
+                print("Scuderia non trovata.")
+        elif scelta == 4:
+            menu()
+        elif scelta == 5:
+            break
+
+def menu_piloti():
+    while True:
+        print("Menu:")
+        print("1. 1 Percentuale vittorie e podi del pilota")
+        print("2. Andamento durata gara nel tempo per un pilota e un circuito specifici")
+        print("3. Andamento durata pit stop nel tempo per un pilota")
+        print("4. Andamento posizione finale nel tempo per un pilota su un circuito specifico")
+        print("5. Andamento posizione media pilota nel tempo")
+        print("6. Torna indietro")
+        print("7. Esci")
+        scelta = int(input("Inserisci il numero corrispondente all'operazione desiderata: "))
+        if scelta == 1:
+                nome_pilota = input("inserisci il nome del pilota: ").strip()
+                cognome_pilota = input("inserisci il cognome del pilota: ").strip()
+                Percentuale_vittorie_pilota(nome_pilota,cognome_pilota)
+        if scelta == 2:
+            nome_pilota = input("Inserisci il nome del pilota: ").strip()
+            cognome_pilota = input("Inserisci il cognome del pilota: ").strip()
+            nome_pista = input("Inserisci il nome del circuito: ").strip()
+            driver_id = Cerca_ID_circuito(nome_pista)
+            circuit_id = Cerca_ID_pilota(nome_pilota,cognome_pilota)
+            if driver_id is not None and circuit_id is not None:
+                grafico_durata_gara(driver_id, circuit_id)
+            else:
+                print("Pilota o circuito non trovato.")
+        elif scelta == 3:
+            nome_pilota = input("Inserisci il nome del pilota: ").strip()
+            cognome_pilota = input("Inserisci il cognome del pilota: ").strip()
+            driver_id = Cerca_ID_pilota(nome_pilota,cognome_pilota)
+            if driver_id is not None:
+                grafico_media_pit_stop(driver_id)
+            else:
+                print("Pilota non trovato.")
+        elif scelta == 4:
+            nome_pilota = input("Inserisci il nome del pilota: ").strip()
+            cognome_pilota = input("Inserisci il cognome del pilota: ").strip()
+            driver_id = Cerca_ID_pilota(nome_pilota,cognome_pilota)
+            circuit_id = Cerca_ID_circuito(nome_pista)
+            if driver_id is not None:
+                grafico_andamento_posizione_circuito(driver_id, circuit_id)
+            else:
+                print("Pilota non trovato.")
+        elif scelta == 5:
+            nome_pilota = input("Inserisci il nome del pilota: ").strip()
+            cognome_pilota = input("Inserisci il cognome del pilota: ").strip()
+            driver_id = Cerca_ID_pilota(nome_pilota,cognome_pilota)
+            if driver_id is not None:
+                grafico_posizione_media(driver_id)
+            else:
+                print("Pilota non trovato.")
+        elif scelta == 6:
+            menu()
+        elif scelta == 7:
+            break
+        else:
+            print("Scelta non valida. Riprova.")
+
 
 def menu():
 
     while True:
-        scelta_categoria=int(input("\nChe categoria vuoi selezionare?\n1.Circuiti,\n2.Scuderie,\n3.Piloti,\n4.Gara,\n5.uscire,\n(inserire numero): ").strip())
+        scelta_categoria=int(input("\nChe categoria vuoi selezionare?\n1.Circuiti,\n2.Scuderie,\n3.Piloti,\n4.Gara,\n5.uscire,\n(inserire numero): "))
         if scelta_categoria==1:
             nome_circuito = input("inserisci il nome del circuito: ").strip()
-            funzioni.Albo_oro_circuito(nome_circuito)
+            Albo_oro_circuito(nome_circuito)
 
         elif scelta_categoria==2:
-            scelta=int(input("quale statistica vuoi vedere?:\n1.Piloti per scuderia,\n(inserire numero): ").strip())
-            if scelta == 1:
-                nome_scuderia = input("inserisci il nome della scuderia: ").strip()
-                funzioni.Piloti_per_scuderia(nome_scuderia)
-
+            menu_scuderia()
+            
         elif scelta_categoria==3:
-            scelta=int(input("quale statistica vuoi vedere?:\n1.Percentuale vittorie e podi del pilota,\n(inserire numero): ").strip())
-            if scelta == 1:
-                nome_pilota = input("inserisci il nome del pilota: ").strip()
-                cognome_pilota = input("inserisci il cognome del pilota: ").strip()
-                funzioni.Percentuale_vittorie_pilota(nome_pilota,cognome_pilota)
+            menu_piloti()
 
         elif scelta_categoria==5:
             print("uscita dal programma")
